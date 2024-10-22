c318b061386a43a30a2898dca642bd910deaaa1c
Gourav6220 <gouravvohra3@gmail.com>
2024-10-22 18:06:51 +0530

    Dto-mapper Category feature implement

--- src/main/java/com/springboot/enotes/Service/CategorySave.java
+++ src/main/java/com/springboot/enotes/Service/CategorySave.java
@@ -2,13 +2,15 @@ package com.springboot.enotes.Service;
 
 import java.util.*;
 
-
-import com.springboot.enotes.Entity.Category;
+import com.springboot.enotes.Dto.CategoryDto;
+import com.springboot.enotes.Dto.CategoryResponse;
 
 public interface CategorySave {
 
-	public boolean saveCategory(Category category);
+	public boolean saveCategory(CategoryDto categoryDto);
 	
-	public List<Category> getAllCategory();
+	public List<CategoryDto> getAllCategory();
+
+	public List<CategoryResponse> getActiveCategory();
 	
 }
--- src/main/java/com/springboot/enotes/ServiceImpl/CategorySaveImpl.java
+++ src/main/java/com/springboot/enotes/ServiceImpl/CategorySaveImpl.java
@@ -1,11 +1,15 @@
 package com.springboot.enotes.ServiceImpl;
 
+import java.util.Date;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.springboot.enotes.Dto.CategoryDto;
+import com.springboot.enotes.Dto.CategoryResponse;
 import com.springboot.enotes.Entity.Category;
 import com.springboot.enotes.Repository.CategoryRepository;
 import com.springboot.enotes.Service.CategorySave;
@@ -17,11 +21,20 @@ public class CategorySaveImpl implements CategorySave {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@Override
-	public boolean saveCategory(Category category) {
+	public boolean saveCategory(CategoryDto categoryDto) {
 
+//		Category category= new Category();
+//		category.setName(categoryDto.getName());
+//		category.setDescription(categoryDto.getDescription());
+//		category.setIsActive(categoryDto.getIsActive());
+		Category category=mapper.map(categoryDto, Category.class);
 		category.setIsDeleted(false);
 		category.setCreatedBy(1);
+		category.setCreatedOn(new Date());
 		Category c=categoryRepository.save(category);
 if(ObjectUtils.isEmpty(c)) {
 	return false;
@@ -31,11 +44,22 @@ if(ObjectUtils.isEmpty(c)) {
 	}
 
 	@Override
-	public List<Category> getAllCategory() {
+	public List<CategoryDto> getAllCategory() {
 	
 		List<Category> allcategory=categoryRepository.findAll();
 	
-		return allcategory;
+		List<CategoryDto> categoryDtoList = allcategory.stream().map(cat->mapper.map(cat, CategoryDto.class)).toList();
+		
+		return categoryDtoList;
+	}
+
+	@Override
+	public List<CategoryResponse> getActiveCategory() {
+
+		List<Category> allcategory=categoryRepository.findByIsActiveTrue();
+		List<CategoryResponse> categorylist = allcategory.stream().map(cat->mapper.map(cat, CategoryResponse.class)).toList();
+		
+		return categorylist;
 	}
 
 }
